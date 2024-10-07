34b2190218e73e955464a4b0f5926e6cc5af3bc3
lydiawright1 <a02361823@aggies.usu.edu>
2024-10-07 10:28:32 -0600

    Create recursion_iteration.py

---
+++ recursion_iteration.py
@@ -0,0 +1,13 @@
+lst = [3, 1, 4, 1, 5, 9, 2, 6]
+
+# for l in lst:
+#     print(l)
+
+#print number
+def print_number(l, index, length):
+    if index == length:
+        return
+    print(l[index])
+    print_number(l, index+1, length)
+
+print_number(lst, 0, len(lst))
