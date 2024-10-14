33b37d8e9cd3d831f547f417798895bea5113eb3
lydiawright1 <a02361823@aggies.usu.edu>
2024-10-14 10:30:06 -0600

    yep

--- week7_sorting_algorithms/O(n^2)_sorts/bubble_sort.py
+++ week7_sorting_algorithms/O(n^2)_sorts/bubble_sort.py
@@ -10,5 +10,5 @@ for j in range(len(lst)-1):
         #compare two elements and swap if elemnt 1 is greater
         if lst[i] > lst[i+1]:
             lst[i], lst[i+1] = lst[i+1], lst[i]
-    # print(lst) # each iteration
+    print(lst) # each iteration
 print("bubble sort: ", lst)
